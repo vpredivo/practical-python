32760998cc4dd2417d5fe7e5a9659239383ec2a0
vpredivo <victor.redivo@loft.com.br>
2020-11-06 15:08:29 -0300

    let the course begin

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+for i in range(10):
+    height = height*3/5
+    print(i+1, round(height,4))
+    
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+month = 0
+
+while principal > 0:
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        principal = principal * (1+rate/12) - (payment+extra_payment)
+        total_paid = total_paid + (payment+extra_payment)
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+    #fix the overpayment
+    if principal < 0:
+        total_paid = total_paid + principal
+        principal = 0
+        
+    month = month + 1
+    print(month, total_paid, principal)
+print('Total paid', total_paid)
+print('Months', month)
